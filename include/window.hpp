997306ae925ccefd90c3a06157e9abdf9f2216f7
Chihiro Kondo <chihirokondo@ChihironoMacBook-ea.local>
2020-09-16 19:24:47 +0900

    Implement main function

---
+++ include/window.hpp
@@ -0,0 +1,49 @@
+#ifndef WANGLANDAU_WINDOW_H_
+#define WANGLANDAU_WINDOW_H_
+
+
+#include <iostream>
+#include "mpi.hpp"
+#include "histo_env_manager.hpp"
+
+
+class WindowManager {
+ public:
+  WindowManager(HistoEnvManager histo_env, MPIV mpiv, double overlap);
+  // Gettor.
+  double valmin() const {return valmin_;}
+  double valmax() const {return valmax_;}
+  size_t imin() const {return imin_;}
+  size_t imax() const {return imax_;}
+  size_t isew() const {return isew_;}
+  size_t iwidth() const {return iwidth_;}
+ private:
+  double valmin_, valmax_;
+  size_t imin_, imax_, isew_, iwidth_;
+};
+
+
+WindowManager::WindowManager(HistoEnvManager histo_env, MPIV mpiv,
+    double overlap) {
+  double width = (histo_env.maxval()-histo_env.minval()) /
+    (1 + (mpiv.num_windows()-1)*(1-overlap));
+  valmin_ = histo_env.minval() + (mpiv.myid()/mpiv.multiple())*(1-overlap)*width;
+  valmax_ = valmin_+width;
+  imin_ = histo_env.GetIndex(valmin_);
+  imax_ = histo_env.GetIndex(valmax_);
+  valmin_ = histo_env.GetVal(imin_, "min");
+  valmax_ = histo_env.GetVal(imax_, "max");
+  if (mpiv.myid() < mpiv.multiple()) {
+    imin_ = 0;
+    valmin_ = histo_env.minval();
+  }
+  if (mpiv.myid() >= mpiv.numprocs()-mpiv.multiple()) {
+    imax_ = histo_env.num_bins()-1;
+    valmax_ = histo_env.maxval();
+  }
+  iwidth_ = imax_ - imin_;
+  isew_ = histo_env.GetIndex(valmin_+width*(1-overlap)) - imin_;
+}
+
+
+#endif // WANGLANDAU_WINDOW_H_
---
+++ include/wl_params.hpp
@@ -0,0 +1,32 @@
+#ifndef WANGLANDAU_WL_PARAMS_H_
+#define WANGLANDAU_WL_PARAMS_H_
+
+
+#include <iostream>
+
+
+class WLParams {
+ public:
+  WLParams(int sweeps, int check_flatness_every, double lnf, double lnfmin,
+      double flatness, int swap_every) :
+    sweeps_(sweeps),
+    check_flatness_every_(check_flatness_every),
+    lnf_(lnf),
+    lnfmin_(lnfmin),
+    flatness_(flatness),
+    swap_every_(swap_every) {}
+  void update_lnf() {lnf_ /= 2.0;}
+  // Gettor.
+  int sweeps() const {return sweeps_;}
+  int check_flatness_every() const {return check_flatness_every_;}
+  double lnf() const {return lnf_;}
+  double lnfmin() const {return lnfmin_;}
+  double flatness() const {return flatness_;}
+  int swap_every() const {return swap_every_;}
+ private:
+  int check_flatness_every_, swap_every_, sweeps_;
+  double lnf_, lnfmin_, flatness_;
+};
+
+
+#endif // WANGLANDAU_WL_PARAMS_H_
